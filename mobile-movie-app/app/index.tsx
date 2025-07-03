790cdc1278a08c284a4996d11d1c97d0aef9a808
Venuja Ranasinghe <it23225510@my.sliit.lk>
2025-07-03 22:53:22 +0530

    Update welcome text color and import Link component
    Changed the welcome text color from blue-500 to dark-200 for improved styling consistency. Also added import for the Link component from expo-router, though it is not yet used.

--- mobile-movie-app/app/index.tsx
+++ mobile-movie-app/app/index.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from "react-native";
+import {Link} from "expo-router";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-blue-500 font-bold">Welcome!!</Text>
+      <Text className="text-5xl text-dark-200 font-bold">Welcome!!</Text>
     </View>
   );
 }
