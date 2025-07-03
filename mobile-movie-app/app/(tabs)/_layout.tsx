3fd617250ec31fa8bf4da38980495b0bfe148dc2
Venuja Ranasinghe <it23225510@my.sliit.lk>
2025-07-03 23:17:23 +0530

    Add tab navigation and movie details screen
    Introduced a tab-based layout under the (tabs) directory with Home, Profile, Saved, and Search screens. Moved the main index to (tabs)/index.tsx. Added a movie details screen at movies/[id].tsx and updated the root layout to support the new tab structure and movie details route.

---
+++ mobile-movie-app/app/(tabs)/_layout.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import {Tabs} from "expo-router";
+
+const _Layout = () => {
+    return (
+        <Tabs>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    headerShown: false }}
+            />
+        </Tabs>
+    );
+};
+
+export default _Layout;
