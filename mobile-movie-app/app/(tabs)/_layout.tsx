aa9054e3980b0f35f2492e9e4cc9ab3ee1ea9692
Venuja Ranasinghe <it23225510@my.sliit.lk>
2025-07-03 23:23:25 +0530

    Add tab screens and asset constants for movie app
    Added new tab screens (search, saved, profile) to the layout. Introduced constants for icons and images, type declarations for image imports, and interfaces for movie-related data structures to support app development.

--- mobile-movie-app/app/(tabs)/_layout.tsx
+++ mobile-movie-app/app/(tabs)/_layout.tsx
@@ -11,6 +11,24 @@ const _Layout = () => {
                     title: 'Home',
                     headerShown: false }}
             />
+            <Tabs.Screen
+                name="search"
+                options={{
+                    title: 'Search',
+                    headerShown: false }}
+            />
+            <Tabs.Screen
+                name="saved"
+                options={{
+                    title: 'Saved',
+                    headerShown: false }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: 'Profile',
+                    headerShown: false }}
+            />
         </Tabs>
     );
 };
