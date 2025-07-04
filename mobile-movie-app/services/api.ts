e0e261a7cd300a056c81a89b56a67261c62f2ea1
Venuja Ranasinghe <it23225510@my.sliit.lk>
2025-07-04 20:21:54 +0530

    Add initial TMDB API fetch example
    Created api.ts with a commented example for fetching movie data from The Movie Database API. This serves as a reference for future API integration.

---
+++ mobile-movie-app/services/api.ts
@@ -0,0 +1,13 @@
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         accept: 'application/json',
+//         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmQ3NjBkZTQ3ZmVjOGY2ZWI4Y2Y3YzZhZWNjN2QwZCIsIm5iZiI6MTc1MTYzMDI2Ny42ODUsInN1YiI6IjY4NjdjMWJiOTdmYjYyZWE5YzY3NzM3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.75zEJPvtp6OfShRdNmt8zhPAhi2UG3Skn0NQHRQiWdw'
+//     }
+// };
+//
+// fetch(url, options)
+//     .then(res => res.json())
+//     .then(json => console.log(json))
+//     .catch(err => console.error(err));
