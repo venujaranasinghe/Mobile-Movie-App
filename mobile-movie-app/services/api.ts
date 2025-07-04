aaa5b45bba565b153aeaf81d624e5e70bc3db80d
Venuja Ranasinghe <it23225510@my.sliit.lk>
2025-07-04 20:43:52 +0530

    Add TMDB API config and fetchMovies function
    Introduces a .env file for the API key and refactors api.ts to export TMDB configuration and a fetchMovies function that supports both search and discovery endpoints. Removes old commented-out fetch code and centralizes API key usage via environment variables.

--- mobile-movie-app/services/api.ts
+++ mobile-movie-app/services/api.ts
@@ -1,13 +1,31 @@
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         accept: 'application/json',
-//         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmQ3NjBkZTQ3ZmVjOGY2ZWI4Y2Y3YzZhZWNjN2QwZCIsIm5iZiI6MTc1MTYzMDI2Ny42ODUsInN1YiI6IjY4NjdjMWJiOTdmYjYyZWE5YzY3NzM3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.75zEJPvtp6OfShRdNmt8zhPAhi2UG3Skn0NQHRQiWdw'
-//     }
-// };
-//
-// fetch(url, options)
-//     .then(res => res.json())
-//     .then(json => console.log(json))
-//     .catch(err => console.error(err));
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+
+    }
+}
+
+export const fetchMovies = async ({ query }: { query: string}) => {
+    const endpoint = query
+        ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers,
+    });
+
+    if(!response.ok) {
+        // @ts-ignore
+        throw new Error('Failed to fetch movies', response.statusText);
+    }
+
+    const data = await response.json();
+
+    return data.results;
+}
+
+// /discover/movie
